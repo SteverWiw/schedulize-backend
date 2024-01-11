5dadd7b26b4a3c22f230ad0e5cb63a0b9cdd79f0
Jhonathan Ruiz <Jhonathansr@arqs.co>
2024-01-11 02:09:43 -0500

    se versiona el proyecto con la CORSConfig se deja un TODO que indica esto 	//TODO: se debe validar el proyecto sin esta funcionalidad se refiere a la Websocket config que se debe quitar y validar que CORS funcione sin Websocketconfig ademas se debe validar como inplementar la configuracion en el springsecurity para quitar los usos deprecados de core().and()

--- src/main/java/com/ajsoftware/backendjwtauth/config/SecurityConfig.java
+++ src/main/java/com/ajsoftware/backendjwtauth/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.ajsoftware.backendjwtauth.config;
 
 import com.ajsoftware.backendjwtauth.jwt.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +15,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -23,11 +32,31 @@ public class SecurityConfig {
 
     @Value("${application.request.mappings}")
     private String url_base;
+    //TODO: se deja en comentario luego se probara esta funcionalidad sin usar la clase independiente
+   /* @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        log.info("<--------------inicia configuracion de CORS --------------->");
+        CorsConfiguration cc = new CorsConfiguration();
+        cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type","Cross-Origin-Resource-Policy", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
+        cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        cc.setAllowedOrigins(Arrays.asList("/*"));
+        cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
+        cc.addAllowedOrigin("http://localhost:4200/");
+        cc.setMaxAge(Duration.ZERO);
+        cc.setAllowCredentials(Boolean.TRUE);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cc);
+        return source;
+    }*/
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize -> authorize
+        log.info("<--------------inicia configuracion de SECURITY --------------->");
+        return http.cors().and().
+                csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(url_base + "/v1/api/auth/**").permitAll()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated())
                 .sessionManagement(SessionManager ->
                         SessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -35,4 +64,6 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
+
 }
--- src/main/java/com/ajsoftware/backendjwtauth/controller/auth/AuthController.java
+++ src/main/java/com/ajsoftware/backendjwtauth/controller/auth/AuthController.java
@@ -7,15 +7,14 @@ import com.ajsoftware.backendjwtauth.interfaces.AuthService;
 import com.ajsoftware.backendjwtauth.response.ResponseRest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+//TODO: se debe validar el proyecto sin esta funcionalidad
+@CrossOrigin(origins = {"/*"})
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @RequestMapping("${application.request.mappings}/v1/api/auth")
